a6c26b71b15dd699eeaafae8e7244c271da5694d
yuuy83 <nghiawapro2@gmail.com>
2024-10-17 20:46:59 +0700

    Q1-3

---
+++ Q3expo/app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import { useState } from "react";
+
+export default () => {
+  const [pressCount, setPressCount] = useState(0);
+  return (
+    <View style={{ alignItems: "center", marginTop: 20 }}>
+      <Text>You've pressed the button: {pressCount} time(s) </Text>
+      <Button
+        title="Press me"
+        onPress={() => setPressCount(pressCount + 1)}
+      />{" "}
+    </View>
+  );
+};
